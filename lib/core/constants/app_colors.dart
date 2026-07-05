53b68e62a453ba005c6c8d142a602c15010aa46d
Hesham122 <hesham@example.com>
2026-07-05 15:35:39 +0300

    AppTheme&AppColors
    -make theme for AppBar
    -make theme for TextButton
    -make them for elevatedButton
    -add colors we needed in login&signUp

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  static const Color backgroundColor = Colors.white;
+  static const Color labelTextColor = Color(0xff535353);
+  static const Color hintTextColor = Color(0xffA6A6A6);
+  static const Color textFieldBorderColor = Color(0xff0F0F0F);
+  static const Color enableButtonColor = Color(0xff02369C);
+  static const Color disableButtonColor = Color(0xff878787);
+  static const Color notValidColor = Color(0xffCC1010);
+}
---
+++ lib/core/ui/theme/app_theme.dart
@@ -0,0 +1,93 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:testly/core/constants/app_colors.dart';
+
+class AppTheme {
+  AppTheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    scaffoldBackgroundColor: AppColors.backgroundColor,
+
+    appBarTheme: AppBarTheme(
+      titleTextStyle: TextStyle(
+        color: AppColors.textFieldBorderColor,
+        fontSize: 20,
+        fontFamily: "Inter",
+        fontWeight: FontWeight.w500,
+      ),
+      elevation: 0,
+      backgroundColor: Colors.white,
+      // foregroundColor: AppColors.textFieldBorderColor,
+      iconTheme: IconThemeData(color: Colors.black, size: 24),
+    ),
+    textButtonTheme: TextButtonThemeData(
+      style: TextButton.styleFrom(
+        textStyle: TextStyle(decoration: TextDecoration.underline),
+        foregroundColor: AppColors.enableButtonColor,
+      ),
+    ),
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: AppColors.enableButtonColor,
+        elevation: 0,
+        disabledBackgroundColor: AppColors.disableButtonColor,
+        foregroundColor: Colors.white,
+
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(100)),
+      ),
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      // error label style
+      floatingLabelStyle: WidgetStateTextStyle.resolveWith((states) {
+        if (states.contains(WidgetState.error)) {
+          return const TextStyle(color: Colors.red);
+        }
+        return const TextStyle(color: AppColors.labelTextColor);
+      }),
+      //  focused error border
+      focusedErrorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide(color: AppColors.notValidColor, width: 1),
+      ),
+      errorStyle: TextStyle(color: AppColors.notValidColor),
+
+      // border when error
+      errorBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide(color: AppColors.notValidColor, width: 1),
+      ),
+
+      // hint and label
+      floatingLabelBehavior: FloatingLabelBehavior.always,
+      hintStyle: TextStyle(color: AppColors.hintTextColor),
+      labelStyle: TextStyle(color: AppColors.labelTextColor),
+      contentPadding: EdgeInsets.only(top: 16, bottom: 16, left: 16),
+
+      // border
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide(color: AppColors.textFieldBorderColor, width: 1),
+      ),
+
+      // enabledBorder
+      enabledBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide(color: AppColors.textFieldBorderColor, width: 1),
+      ),
+
+      // foucsedBorder
+      focusedBorder: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide(color: AppColors.textFieldBorderColor, width: 1),
+      ),
+    ),
+
+    // cardTheme: CardThemeData(
+    //   elevation: 2,
+    //   shape: RoundedRectangleBorder(
+    //     borderRadius: BorderRadius.circular(16),
+    //   ),
+    // ),
+  );
+}
