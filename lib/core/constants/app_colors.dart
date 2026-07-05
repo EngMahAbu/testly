3479ce17121ed72fdff15537ed9e61686a322102
Hesham122 <hesham@example.com>
2026-07-05 15:35:39 +0300

    AppTheme&AppColors
    -make theme for AppBar
    -make theme for TextButton
    -make them for elevatedButton
    -add colors we needed in login&signUp

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,4 +1,4 @@
-import 'dart:ui';
+import 'package:flutter/material.dart';
 
 abstract final class AppColors {
   static const Color white = Color(0xFFF9F9F9);
@@ -6,4 +6,11 @@ abstract final class AppColors {
   static const Color grey = Color(0xFFA6A6A6);
   static const Color darkGrey = Color(0xFF535353);
   static const Color blue = Color(0xFF02369C);
+  static const Color backgroundColor = Colors.white;
+  static const Color labelTextColor = Color(0xff535353);
+  static const Color hintTextColor = Color(0xffA6A6A6);
+  static const Color textFieldBorderColor = Color(0xff0F0F0F);
+  static const Color enableButtonColor = Color(0xff02369C);
+  static const Color disableButtonColor = Color(0xff878787);
+  static const Color notValidColor = Color(0xffCC1010);
 }
--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
   configureDependencies();
   runApp(const MyApp());
 }
-    
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
