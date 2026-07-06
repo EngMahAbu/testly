428323b9b921f31b457929dff33d98914843968f
Hesham122 <hesham@example.com>
2026-07-06 23:21:21 +0300

    edit ThemeData and add constant file

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -1,16 +1,12 @@
 import 'package:flutter/material.dart';
 
 abstract class AppColors {
-  static const Color white = Color(0xFFF9F9F9);
-  static const Color black = Color(0xFF0F0F0F);
-  static const Color grey = Color(0xFFA6A6A6);
-  static const Color darkGrey = Color(0xFF535353);
-  static const Color blue = Color(0xFF02369C);
-  static const Color backgroundColor = Colors.white;
-  static const Color labelTextColor = Color(0xff535353);
-  static const Color hintTextColor = Color(0xffA6A6A6);
-  static const Color textFieldBorderColor = Color(0xff0F0F0F);
-  static const Color blueBaseColor = Color(0xff02369C);
-  static const Color disableButtonColor = Color(0xff878787);
-  static const Color notValidColor = Color(0xffCC1010);
+  static const Color white = Colors.white;
+  static const Color black = Colors.black;
+  static const Color darkGrey = Color(0xff535353);
+  static const Color grey = Color(0xffA6A6A6);
+  static const Color blackShade = Color(0xff0F0F0F);
+  static const Color blue = Color(0xff02369C);
+  static const Color greyShade = Color(0xff878787);
+  static const Color lightRed = Color(0xffCC1010);
 }
