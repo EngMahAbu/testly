cbb2340508b03fa63892899263b7e7146e31fdb1
Hesham122 <hesham@example.com>
2026-07-06 23:21:21 +0300

    edit ThemeData and add constant file

--- lib/core/ui/theme/app_theme.dart
+++ lib/core/ui/theme/app_theme.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:testly/core/constants/app_colors.dart';
+import 'package:testly/core/constants/constant.dart';
 
 class AppTheme {
   AppTheme._();
 
   static ThemeData lightTheme = ThemeData(
-    scaffoldBackgroundColor: AppColors.white,
 
+       fontFamily:AppConstant.fontApp,
+
+    scaffoldBackgroundColor: AppColors.white,
+ 
     appBarTheme: AppBarTheme(
+
       titleTextStyle: TextStyle(
-        color: AppColors.textFieldBorderColor,
+        color: AppColors.blackShade,
         fontSize: 20,
-        fontFamily: "Inter",
+        fontFamily: AppConstant.fontApp,
         fontWeight: FontWeight.w500,
       ),
       elevation: 0,
@@ -22,14 +27,14 @@ class AppTheme {
     textButtonTheme: TextButtonThemeData(
       style: TextButton.styleFrom(
         textStyle: TextStyle(decoration: TextDecoration.underline),
-        foregroundColor: AppColors.blueBaseColor,
+        foregroundColor: AppColors.blue,
       ),
     ),
     elevatedButtonTheme: ElevatedButtonThemeData(
       style: ElevatedButton.styleFrom(
-        backgroundColor: AppColors.blueBaseColor,
+        backgroundColor: AppColors.blue,
         elevation: 0,
-        disabledBackgroundColor: AppColors.disableButtonColor,
+        disabledBackgroundColor: AppColors.greyShade,
         foregroundColor: AppColors.white,
       ),
     ),
@@ -37,39 +42,42 @@ class AppTheme {
       // error label style
       floatingLabelStyle: WidgetStateTextStyle.resolveWith((states) {
         if (states.contains(WidgetState.error)) {
-          return const TextStyle(color:AppColors.notValidColor);
+          return const TextStyle(color:AppColors.lightRed);
         }
-        return const TextStyle(color: AppColors.labelTextColor);
+        return const TextStyle(color: AppColors.darkGrey);
       }),
       //  focused error border
       focusedErrorBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(4),
-        borderSide: BorderSide(color: AppColors.notValidColor, width: 1),
+        borderSide: BorderSide(color: AppColors.lightRed, width: 1),
       ),
-      errorStyle: TextStyle(color: AppColors.notValidColor),
+      errorStyle: TextStyle(color: AppColors.lightRed),
 
       // border when error
       errorBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(4),
-        borderSide: BorderSide(color: AppColors.notValidColor, width: 1),
+        borderSide: BorderSide(color: AppColors.lightRed, width: 1),
       ),
 
       // hint and label
       floatingLabelBehavior: FloatingLabelBehavior.always,
-      hintStyle: TextStyle(color: AppColors.hintTextColor),
-      labelStyle: TextStyle(color: AppColors.labelTextColor),
+      hintStyle: TextStyle(color: AppColors.grey),
+      labelStyle: TextStyle(color: AppColors.darkGrey),
       contentPadding: EdgeInsets.only(top: 16, bottom: 16, left: 16),
   
       // border
       border: OutlineInputBorder(
         borderRadius: BorderRadius.circular(4),
-        borderSide: BorderSide(color: AppColors.textFieldBorderColor, width: 1),
+        borderSide: BorderSide(color: AppColors.blackShade, width: 1.3),
       ),
-   
+     enabledBorder:  OutlineInputBorder(
+        borderRadius: BorderRadius.circular(4),
+        borderSide: BorderSide(color: AppColors.blackShade, width: 1.3),
+      ),
       // foucsedBorder
       focusedBorder: OutlineInputBorder(
         borderRadius: BorderRadius.circular(4),
-        borderSide: BorderSide(color: AppColors.textFieldBorderColor, width: 1),
+        borderSide: BorderSide(color: AppColors.blackShade, width: 1.6,),
       ),
     ),
 
