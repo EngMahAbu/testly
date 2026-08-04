38066bddc4058527b02d2398a383440c619535a0
Hesham122 <hesham@example.com>
2026-08-04 22:57:48 +0300

    Implement authentication feature with API client, data sources, models, and use cases

---
+++ lib/features/auth/api/client/auth_api_client.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:injectable/injectable.dart';
+import 'package:retrofit/error_logger.dart';
+import 'package:retrofit/http.dart';
+import 'package:testly/core/constants/endpoints.dart';
+import 'package:testly/features/auth/data/models/login_request.dart';
+import 'package:testly/features/auth/data/models/login_response.dart';
+import 'package:testly/features/auth/data/models/signup_request.dart';
+import 'package:testly/features/auth/data/models/signup_response.dart';
+
+part 'auth_api_client.g.dart';
+
+@singleton
+@RestApi()
+abstract class AuthApiClient {
+  @factoryMethod
+  factory AuthApiClient(Dio dio) = _AuthApiClient;
+  
+  @POST(Endpoints.signIn)
+  Future<LoginResponse> login(@Body() LoginRequest request);
+   
+
+
+ 
+
+  @POST(Endpoints.signup)
+  Future<SignupResponse> signup(@Body() SignupRequest signupRequest);
+}
